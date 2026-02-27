0829de624f6c8b53ea6754302ac7a5407d1585ce
sakashinakamoto <128051779+sakashinakamoto@users.noreply.github.com>
2026-02-27 14:56:40 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,3 +1,10 @@
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
+    "Accept": "application/json",
+    "Referer": "https://app.prizepicks.com/",
+    "Origin": "https://app.prizepicks.com"
+}
+
 import os, math, logging, requests, threading
 from datetime import datetime
 from flask import Flask
